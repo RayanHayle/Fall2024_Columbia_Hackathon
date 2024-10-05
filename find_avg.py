b43eb184ce11810f6f4d69564b34f44ce3f383b6
Rayan Ali Hayle <rha4542@gmail.com>
2024-10-05 17:21:37 -0400

    Update find_avg.py

--- find_avg.py
+++ find_avg.py
@@ -6,9 +6,9 @@ import pandas as pd
 def Read_Unemployment_rate():
   my_df = pd.read_csv('data/unemployment_rate .csv') # stores data in my_df, each column has a coressponding header
 
+  
 # so now create a new column that has the avg of all the columns in each row\
-  my_df['Avg_year'] = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-  my_df.mean(axis=1) # find the mean of all 12 columns 
-  print( my_df)
-
+  month_columns = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+  my_df['Avg_Year'] = month_columns.mean(axis=1)
+  print(my_df)
 Read_Unemployment_rate()
