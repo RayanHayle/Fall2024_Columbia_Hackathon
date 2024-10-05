a842578ac03cadcf057d3c8dc644a0b10da73b48
Rayan Ali Hayle <rha4542@gmail.com>
2024-10-05 16:50:14 -0400

    Update find_avg.py

--- find_avg.py
+++ find_avg.py
@@ -1,1 +1,11 @@
+# part 1: find csv data 
+#Part 2: analysis the data ---> summarize the past data as growth or recession based on the average.
+import csv
+import pandad as pd
 
+
+def Read_Unemployment_rate():
+  my_df = pd.read_csv('data/unemployment_rate .csv') # unemployment_rate .css is in data path
+  print(my_df.head()) # top 5 lines
+
+Read_Unemployment_rate()
