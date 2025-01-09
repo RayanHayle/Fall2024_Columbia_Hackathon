79a591a7c476598a96dca0665f634861a2d30781
Rayan Ali Hayle <rha4542@gmail.com>
2025-01-09 10:00:12 -0500

    Update Predict.py

--- Predict.py
+++ Predict.py
@@ -1,5 +1,7 @@
-
-# part 3: train the model to predict  
+# Part 1: Find the data ✅
+# Part 2: Analyze the data ✅
+# part 3: train the model to predict based on the data.  I will do this over spring semester. 
 
 import pandas as pd
 import matplotlib.pyplot as plt
+
