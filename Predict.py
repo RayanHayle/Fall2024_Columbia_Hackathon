668397d0ddf69328f8061804c39592e1e3917a8b
Rayan Ali Hayle <rha4542@gmail.com>
2025-04-02 16:28:50 -0400

    Delete Predict.py

--- Predict.py
+++
@@ -1,7 +0,0 @@
-# Part 1: Find the data ✅
-# Part 2: Analyze the data ✅
-# part 3: train the model to predict based on the data.  I will do this over spring semester. 
-
-import pandas as pd
-import matplotlib.pyplot as plt
-
